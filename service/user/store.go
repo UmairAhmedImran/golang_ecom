4937e2fcb128db38eb619ff0576a954136b9639c
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2025-01-29 10:43:36 +0500

    RegisterUser more

---
+++ service/user/store.go
@@ -0,0 +1,20 @@
+package user
+
+import (
+	"database/sql"
+
+	"github.com/UmairAhmedImran/ecom/types"
+)
+
+type Store struct {
+  db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+  return &Store{db: db}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+
+
+}
--- types/types.go
+++ types/types.go
@@ -1,5 +1,14 @@
 package types
 
+type User struct {
+  ID          int64   `json:"id"`
+  FirstName   string `json:"firstName"`
+  LastName    string  `json:"lastName"`
+  Email       string `json:"email"`
+  Password    string`json:"-"`
+  CreatedAt string `json:"createdAt"`
+}
+
 type RegisterUserPayload struct {
   FirstName   string `json:"firstName"`
   LastName    string  `json:"lastName"`
