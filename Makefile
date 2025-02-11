63974ca6efa8a90ac52906dec7522f66a6a476b9
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2025-02-12 01:00:53 +0500

    Working on DB

--- Makefile
+++ Makefile
@@ -1,8 +1,46 @@
+include .env
+MIGRATION_PATH = ./cmd/migrate/migrations
+
+create_migrations:
+	sqlx migrate add -r init
+migrate-up:
+	sqlx migrate run --database-url "postgres://${DB_USER}:${PASSWD}@${HOST}:${PORT}/${DB_NAME}?sslmode=disable"
+migrate-down:
+	sqlx migrate revert --database-url "postgres://${DB_USER}:${PASSWD}@${HOST}:${PORT}/${DB_NAME}?sslmode=disable"
+stop_containers:
+	@echo "Stopping other docker container"
+	if [ $$(docker ps -q) ]; then \
+		echo "found and stopped containers"; \
+		docker stop $$(docker ps -q); \
+	else \
+		echo "no containers running..."; \
+	fi
+
+create_container:
+	docker run --name ${DB_DOCKER_CONTAINER} -p 5436:5433 -e POSTGRES_USER=${DB_USER} -e POSTGRES_PASSWORD=${PASSWD} -d postgres:17-alpine
+
+create_db:
+	docker exec -it ${DB_DOCKER_CONTAINER} createdb --username=${DB_USER} --owner=${DB_USER} ${DB_NAME}
+
+start_container:
+	docker start ${DB_DOCKER_CONTAINER}
+
 build:
-	@go build -o bin/ecom cmd/main.go
+	if [ -f "${BINARY}" ]; then \
+		rm ${BINARY}; \
+		echo "Deleted ${BINARY}"; \
+	fi
+	@echo "Building binary..."
+	go build -o ${BINARY} cmd/main.go
+
+run: build
+	./${BINARY}
+#@@echo "api started..."
+
+stop:
+	@echo "stopping server..."
+	@-pkill -SIGTERM -f "./${BINARY}"
+	@echo "server stopped..."
 
 test:
 	@go test -v ./...
-
-run: build 
-	@./bin/ecom
--- db/db.go
+++ db/db.go
@@ -3,31 +3,44 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/UmairAhmedImran/ecom/config"
-	_ "github.com/lib/pq"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
+const MaxOpenDbConn = 10
+const MaxIdleDbConn = 5
+const MaxDbLifetime = 5 * time.Minute
+
 func NewPostgresStorage() (*sql.DB, error) {
-	user := config.GetEnv("DB_USER", "postgres")
-	dbName := config.GetEnv("DB_NAME", "postgres")
-	password := config.GetEnv("PASSWD", "ecompassword")
-	port := config.GetEnv("PORT", "5437")
-	sslMode := config.GetEnv("SSL_MODE", "disable")
+	// user := config.GetEnv("DB_USER", "postgres")
+	// dbName := config.GetEnv("DB_NAME", "postgres")
+	// password := config.GetEnv("PASSWD", "ecompassword")
+	// port := config.GetEnv("PORT", "5436")
+	// sslMode := config.GetEnv("SSL_MODE", "disable")
+	//
+	// connStr := fmt.Sprintf(
+	// 	"user=%s dbname=%s password=%s port=%s sslmode=%s",
+	// 	user, dbName, password, port, sslMode,
+	// )
+  
+  	connStr := config.GetEnv("DB_URL", "")
 
-	connStr := fmt.Sprintf(
-		"user=%s dbname=%s password=%s port=%s sslmode=%s",
-		user, dbName, password, port, sslMode,
-	)
+	db, err := sql.Open("pgx", connStr)
+ 	if err != nil {
+ 		return nil, err
+	 }
 
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		return nil, err
-	}
+	 db.SetMaxOpenConns(MaxOpenDbConn)
+	 db.SetMaxIdleConns(MaxIdleDbConn)
+	 db.SetConnMaxLifetime(MaxDbLifetime)
 
 	if err := db.Ping(); err != nil {
 		return nil, err
 	}
+
+	fmt.Println("*** Database connection established ***")
 
 	return db, nil
 }
